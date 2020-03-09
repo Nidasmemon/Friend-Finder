3243c15dcb680c2a8c5673febed2ade5b2ae0688
Nida Memon <nidasmemon@gmail.com>
2020-03-09 00:36:11 -0700

    completed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,38 @@ module.exports = function (app) {
 
         var newFriend = req.body;
         friends.push(newFriend)
-        res.json(friends)
+        // res.json(friends)
 
-        console.log(req.body);
-        console.log("Hello World");
+        console.log(newFriend);
+        var arr;
+        var sumOfScores = [];
+        var diffSum = [];
+
+
+        for (var i = 0; i < friends.length - 1; i++) {
+
+            arr = [];
+
+            for (var j = 0; j < 10; j++) {
+
+                scoreDiff = (newFriend.scores[j] - friends[i].scores[j]);
+                scoreDiff = Math.abs(scoreDiff);
+                arr.push(scoreDiff);
+            }
+            
+            var sum = 0;
+            for (var k = 0; k < arr.length; k++) {
+                sum += arr[k];
+            }
+            diffSum.push(sum);
+        }
+
+        var myMinIndex = diffSum.indexOf(Math.min(...diffSum));
+
+        // console.log("The value of the index with the smallest number is: " + myMinIndex);
+        // console.log("The min value is: " + Math.min(...diffSum));
+        // console.log("The value of diffSum is: " + diffSum);
+
+        res.json(friends[myMinIndex]);
     })
 }
